066870e1275bf37c174e801a034c06b0755cc39e
fabyDiaz <fbl.diaz.n@gmail.com>
2024-04-26 17:15:33 -0400

    se soluciono el cuadro de notas, empezamos profile page

---
+++ app/src/main/java/com/example/alkewalletmodulo4/Fragmentos/UltimasTransacciones.java
@@ -0,0 +1,33 @@
+package com.example.alkewalletmodulo4.Fragmentos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.alkewalletmodulo4.R;
+
+public class UltimasTransacciones extends Fragment {
+
+    public UltimasTransacciones() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ultimas_transacciones, container, false);
+    }
+}
